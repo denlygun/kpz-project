5ec9a0331b79ec07949217659977725257711e36
merelythesame <ipz232_pov@student.ztu.edu.ua>
2025-06-06 09:59:56 +0300

    [REFACTOR] Created FoodEntryCommandBase and refactored AddFoodEntryCommand, DeleteFoodEntryCommand

--- CalorieTracker/Services/DeleteFoodEntryCommand.cs
+++ CalorieTracker/Services/DeleteFoodEntryCommand.cs
@@ -8,42 +8,32 @@ using System.Threading.Tasks;
 
 namespace CalorieTracker.Services
 {
-    public class DeleteFoodEntryCommand : ICommand
+    public class DeleteFoodEntryCommand : FoodEntryCommandBase
     {
-        private readonly FoodEntryRepository _repository;
         private readonly int _entryId;
-        private readonly ICalorieGoalSubject _notificationService;
-        private FoodEntry _deletedEntry;
 
         public DeleteFoodEntryCommand(FoodEntryRepository repository, int entryId, ICalorieGoalSubject notificationService)
+            : base(repository, notificationService)
         {
-            _repository = repository;
             _entryId = entryId;
-            _notificationService = notificationService;
         }
 
-        public void Execute()
+        protected override void ExecuteInternal()
         {
-            _deletedEntry = _repository.GetById(_entryId);
-            if (_deletedEntry != null)
+            Entry = Repository.GetById(_entryId);
+            if (Entry != null)
             {
-                var date = _deletedEntry.DateTime;
-                _repository.Delete(_entryId);
-                _repository.SaveChanges();
-                var totalCalories = _repository.GetTotalCaloriesForDate(date);
-                _notificationService.NotifyDailyCaloriesChanged(date, totalCalories);
+                Repository.Delete(_entryId);
             }
         }
 
-        public void Undo()
+        protected override void UndoInternal()
         {
-            if (_deletedEntry != null)
+            if (Entry != null)
             {
-                _repository.Add(_deletedEntry);
-                _repository.SaveChanges();
-                var totalCalories = _repository.GetTotalCaloriesForDate(_deletedEntry.DateTime);
-                _notificationService.NotifyDailyCaloriesChanged(_deletedEntry.DateTime, totalCalories);
+                Repository.Add(Entry);
             }
         }
     }
+
 }
---
+++ CalorieTracker/Services/FoodEntryCommandBase.cs
@@ -0,0 +1,45 @@
+﻿using CalorieTracker.Models;
+using CalorieTracker.Repositories;
+
+namespace CalorieTracker.Services
+{
+    public abstract class FoodEntryCommandBase : ICommand
+    {
+        protected readonly FoodEntryRepository Repository;
+        protected readonly ICalorieGoalSubject NotificationService;
+        protected FoodEntry Entry;
+
+        protected FoodEntryCommandBase(FoodEntryRepository repository, ICalorieGoalSubject notificationService)
+        {
+            Repository = repository;
+            NotificationService = notificationService;
+        }
+
+        public void Execute()
+        {
+            ExecuteInternal();
+            Repository.SaveChanges();
+            Notify();
+        }
+
+        public void Undo()
+        {
+            UndoInternal();
+            Repository.SaveChanges();
+            Notify();
+        }
+
+        protected void Notify()
+        {
+            if (Entry != null)
+            {
+                var totalCalories = Repository.GetTotalCaloriesForDate(Entry.DateTime);
+                NotificationService.NotifyDailyCaloriesChanged(Entry.DateTime, totalCalories);
+            }
+        }
+
+        protected abstract void ExecuteInternal();
+        protected abstract void UndoInternal();
+    }
+
+}
